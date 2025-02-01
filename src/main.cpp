35502132e810e9a3d9a5ff3b19d2ba7abc541a67
Watz Emil Johann Lukas <Watz.Emil@student.htlwrn.ac.at>
2025-02-01 13:05:47 +0100

    Add first draft of VariableClauseRelation class

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,21 @@
 // Created by Emil Watz on 01.02.2025.
 //
 
+#include "VariableClauseRelation.h"
+
 #include <iostream>
+
+using namespace std;
 
 int main() {
     std::cout << "Hello World" << std::endl;
+    VariableClauseRelation f;
+    long c1 = f.addClause();
+    long c2 = f.addClause();
+    long v1 = f.addNewVariableToClause(c1, true);
+    long v2 = f.addNewVariableToClause(c1, false);
+
+    for (auto& c : f.getVariablesOfClause(c1)) {
+        cout << c.first << "; " << c.second << endl;
+    }
 }
